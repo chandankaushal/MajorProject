41d40bc86057ed518d0372c742008620cd2539a7
Chandan <chandan.kaushal@instacart.com>
2024-11-11 07:35:01 -0500

    adding review schema in schema.js

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const listingSchema = Joi.object({
+module.exports.listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
@@ -15,4 +15,13 @@ const listingSchema = Joi.object({
   }).required(), // Ensure listing is a required object
 });
 
-module.exports = listingSchema;
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().min(1).max(5),
+    comment: Joi.string().required(),
+  }).required(),
+});
+
+
+
+
